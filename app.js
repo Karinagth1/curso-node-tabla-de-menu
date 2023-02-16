9007fb4b1de9955085b365398402111e0eb26808
Karina Pineda <karina_pineda97@hotmail.com>
2023-02-16 10:17:35 -0500

    hola mundo

---
+++ app.js
@@ -0,0 +1,7 @@
+// const nombre = 'Karina';
+// const nombre1 = 'Julian';
+// console.log(nombre1);
+console.log('hola mundo y karina ');
+
+
+
---
+++ app2.js
@@ -0,0 +1,6 @@
+const saludar=(nombre) =>{
+    return`Saludos ${nombre}`; //'Saluddos' + nombre;    
+}
+
+
+console.log( saludar('Karina' ));
